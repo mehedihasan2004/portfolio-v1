b2dfb8672b9b034ea20951965ff91578d4968792
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-22 00:25:00 +0600

    style: call the providers in main layout and enable dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
 import type { Metadata } from "next";
+import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,14 +11,14 @@ export const metadata: Metadata = {
   description: "A intovert developer based in Bangladesh.",
 };
 
-type Props = Readonly<{
-  children: ReactNode;
-}>;
+type Props = Readonly<{ children: ReactNode }>;
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
