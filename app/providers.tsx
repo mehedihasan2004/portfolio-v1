1e6b43866853cbbf716711a017463f3f1c1a503e
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-22 00:21:47 +0600

    chore: setup providers

---
+++ app/providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ReactNode } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+
+type Props = { children: ReactNode };
+
+export function Providers({ children }: Props) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
