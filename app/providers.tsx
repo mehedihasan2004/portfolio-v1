ff1b711f886b37478c7b3b533611edfff12c5989
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-22 06:57:42 +0600

    style: add theme provider
    we are using next theme provider as our theme provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
 import { NextUIProvider } from "@nextui-org/react";
 
 type Props = { children: ReactNode };
 
 export function Providers({ children }: Props) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        {children}
+      </ThemeProvider>
+    </NextUIProvider>
+  );
 }
