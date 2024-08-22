4b9982ff27ad616292f603e22ab4230971a13cbe
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-22 07:05:20 +0600

    chore: add motion tag in a structural way

---
+++ lib/motion.ts
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+
+const { h1, h2, h3, h4, h5, h6, p, small, a, div, span } = motion;
+
+export {
+  p as P,
+  a as A,
+  h1 as H1,
+  h2 as H2,
+  h3 as H3,
+  h4 as H4,
+  h5 as H5,
+  h6 as H6,
+  div as Div,
+  span as Span,
+  small as Small,
+};
