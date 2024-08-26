ff82871318214c3d8ccf16c94b4ef59e75305768
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-26 22:20:03 +0600

    feat: add icons in leftbar

--- components/layout/leftbar.tsx
+++ components/layout/leftbar.tsx
@@ -1,5 +1,19 @@
+import { RiLinkedinBoxFill } from "react-icons/ri";
+import { LuGithub, LuInstagram, LuTwitter } from "react-icons/lu";
+
+const iconClassName: string =
+  "cursor-pointer ease-in-out transition duration-300 hover:text-cyan-400";
+
 export function Leftbar() {
   return (
-    <aside className="bg-red-400 w-[30px] h-full fixed top-0 left-[calc((100%-1280px)/2)] z-[300]"></aside>
+    <aside className="w-[30px] h-full fixed top-0 left-[calc((100%-1280px)/2)] z-[300] content-end">
+      <div className="flex flex-col justify-end items-center gap-y-4">
+        <LuGithub size={20} className={iconClassName} />
+        <LuInstagram size={20} className={iconClassName} />
+        <LuTwitter size={20} className={iconClassName} />
+        <RiLinkedinBoxFill size={20} className={iconClassName} />
+        <div className="h-[100px] w-[1px] bg-white" />
+      </div>
+    </aside>
   );
 }
