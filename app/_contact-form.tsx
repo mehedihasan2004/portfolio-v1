81310c8f6deedc8d1e5f1276ac41320522e13907
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-03 05:49:56 +0600

    feat: add contact form

---
+++ app/_contact-form.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@nextui-org/react";
+
+export function ContactForm() {
+  return (
+    <form action="">
+      <Input type="email" variant="underlined" label="Email" />
+    </form>
+  );
+}
