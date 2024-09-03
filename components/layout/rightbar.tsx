90bb00036d158c769333c61888d84a58857c357a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-03 06:32:28 +0600

    feat: add the email to right bar

--- components/layout/rightbar.tsx
+++ components/layout/rightbar.tsx
@@ -1,5 +1,17 @@
+const email = process.env.NEXT_PUBLIC_MY_EMAIL;
+
 export function Rightbar() {
   return (
-    <aside className="bg-sky-400 w-[30px] h-full fixed top-0 right-[calc((100%-1280px)/2)] z-[300]"></aside>
+    <aside className="w-[30px] h-full fixed top-0 right-[calc((100%-1280px)/2)] z-[300] content-end">
+      <div className="flex flex-col justify-end items-center gap-y-4">
+        <a
+          href={"mailto:" + email}
+          className="vertical-text ease-in-out transition duration-300 hover:text-cyan-400 cursor-pointer"
+        >
+          {email}
+        </a>
+        <div className="h-[100px] w-[1px] bg-white" />
+      </div>
+    </aside>
   );
 }
