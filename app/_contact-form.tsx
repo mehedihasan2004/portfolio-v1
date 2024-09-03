223d60ed7e558580c6dd495d723f728d03256eba
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-03 06:13:03 +0600

    feat: add fields for contact form

--- app/_contact-form.tsx
+++ app/_contact-form.tsx
@@ -1,9 +1,24 @@
 import { Input } from "@nextui-org/react";
+import { Textarea } from "@nextui-org/react";
 
 export function ContactForm() {
   return (
-    <form action="">
-      <Input type="email" variant="underlined" label="Email" />
-    </form>
+    <div className="">
+      <h4>Let&apos;s work together</h4>
+      <div className="lg:flex items-center h-full">
+        <form action="" className="space-y-4 w-full">
+          <Input type="name" variant="underlined" label="Your name" />
+          <Input type="email" variant="underlined" label="Your email" />
+
+          <Textarea
+            variant="underlined"
+            label="Message"
+            labelPlacement="outside"
+            placeholder="Enter your message"
+            className="col-span-12 md:col-span-6 mb-6 md:mb-0"
+          />
+        </form>
+      </div>
+    </div>
   );
 }
--- app/_contact.tsx
+++ app/_contact.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Div } from "@/lib/motion";
+import { ContactForm } from "./_contact-form";
 import { slideIn } from "@/lib/motion-animations";
 import { EarthCanvas } from "@/components/canvas/earth-canvas";
 
@@ -9,8 +10,8 @@ export function Contact() {
     <section id="#contact">
       <h4 className="tracking-wide">Contact</h4>
 
-      <div className="grid grid-cols-2 gap-x-4">
-        <div>1</div>
+      <div className="max-lg:flex flex-col-reverse lg:grid grid-cols-2 gap-x-4">
+        <ContactForm />
         <Div
           variants={slideIn("right", "tween", 0.2, 1)}
           className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
