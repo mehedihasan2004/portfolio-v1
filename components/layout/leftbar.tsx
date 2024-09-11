e33f8577b2fb5f91f228b93696862ea793baad11
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-11 16:23:32 +0600

    feat: add twitter x icon in leftbar

--- components/layout/leftbar.tsx
+++ components/layout/leftbar.tsx
@@ -1,3 +1,4 @@
+import { FaXTwitter } from "react-icons/fa6";
 import { RiLinkedinBoxFill } from "react-icons/ri";
 import { LuGithub, LuInstagram, LuTwitter } from "react-icons/lu";
 
@@ -8,8 +9,9 @@ export function Leftbar() {
   return (
     <aside className="w-[30px] h-full fixed top-0 left-[calc((100%-1444px)/2)] z-[300] content-end border">
       <div className="flex flex-col justify-end items-start">
-        <div className="flex flex-col justify-end items-center gap-y-4">
+        <div className="flex flex-col justify-end items-center gap-y-6">
           <LuGithub size={20} className={iconClassName} />
+          <FaXTwitter size={18} className={iconClassName} />
           <LuInstagram size={20} className={iconClassName} />
           <LuTwitter size={20} className={iconClassName} />
           <RiLinkedinBoxFill size={20} className={iconClassName} />
